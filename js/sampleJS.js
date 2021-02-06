51741dbd418e78434f0d06eaff86ef33f6f1bf38
Yuexin <yuexinc@uchicago.edu>
2021-02-06 14:56:58 -0600

    made "project" dropdown disappear

--- js/sampleJS.js
+++ js/sampleJS.js
@@ -28,14 +28,8 @@ function evalNumber(){
     var inputValue = prompt("Enter any five-digit number without commas")
     if (isNaN(inputValue)) { //if the input value is not a number,
       alert(inputValue + " is not a number.");
-    } else if (Number.isInteger(inputValue) == false){
-      //alert(inputValue + " is not an integer.");
-    //} else if (inputValue.length != 5){ //if the length of the number is not 5,
-      //alert(inputValue + " is not a five-digit number.");
-    } else if (inputValue.length > 6 || inputValue.length < 5){
-        alert(inputValue + " is not a five or six-digit number.");
-    } else if (inputValue.length == 6 && inputValue > 0){
-        alert(inputValue + " is not a five-digit number.");
+    } else if (inputValue.length != 5){ //if the length of the number is not 5,
+      alert(inputValue + " is not a five-digit number.");
     } else if (inputValue % 2 == 0){
         alert(inputValue + " is an even number.");
     } else {
@@ -43,7 +37,20 @@ function evalNumber(){
     }
 }
 
+ /*  } else if (Number.isInteger(inputValue) == false){
+      //alert(inputValue + " is not an integer."); */
+ /*  } else if (inputValue.length > 6 || inputValue.length < 5){
+        alert(inputValue + " is not a five or six-digit number.");
+    } else if (inputValue.length == 6 && inputValue > 0){
+        alert(inputValue + " is not a five-digit number."); */
+
+
 function minOfDay(){
     var currentMinute = new Date().getHours() * 60 + new Date().getMinutes();
     alert("Current Minute of the Day: " + currentMinute);
+}
+
+function disappear(){
+    var dropdown = document.getElementById('navbarDropdown')
+    dropdown.style.display = "none";
 }
