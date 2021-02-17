384172656c259758c590b5b36edf8acdf0ca7f27
Yuexin <yuexinc@uchicago.edu>
2021-02-17 11:54:44 -0600

    changed bg color

--- js/sampleJS.js
+++ js/sampleJS.js
@@ -1,9 +1,9 @@
-function howdy(){
+function howdy() {
     var inputValue = prompt("Please enter your first name: ")
-    alert("Howdy "+ inputValue);
+    alert("Howdy " + inputValue);
 }
 
-function conditional(){
+function conditional() {
     alert("Use Inspect to see the console and inspect the code.");
     var currentHour = new Date().getHours();
     if (currentHour < 10) {
@@ -24,33 +24,33 @@ function conditional(){
     }
 } */
 
-function evalNumber(){
+function evalNumber() {
     var inputValue = prompt("Enter any five-digit number without commas")
     if (isNaN(inputValue)) { //if the input value is not a number,
-      alert(inputValue + " is not a number.");
-    } else if (inputValue.length != 5){ //if the length of the number is not 5,
-      alert(inputValue + " is not a five-character entity.");
-    } else if (inputValue % 2 == 0){
+        alert(inputValue + " is not a number.");
+    } else if (inputValue.length != 5) { //if the length of the number is not 5,
+        alert(inputValue + " is not a five-character entity.");
+    } else if (inputValue % 2 == 0) {
         alert(inputValue + " is an even number.");
     } else {
         alert(inputValue + " is an odd number.")
     }
 }
 
- /*  } else if (Number.isInteger(inputValue) == false){
-      //alert(inputValue + " is not an integer."); */
- /*  } else if (inputValue.length > 6 || inputValue.length < 5){
-        alert(inputValue + " is not a five or six-digit number.");
-    } else if (inputValue.length == 6 && inputValue > 0){
-        alert(inputValue + " is not a five-digit number."); */
+/*  } else if (Number.isInteger(inputValue) == false){
+     //alert(inputValue + " is not an integer."); */
+/*  } else if (inputValue.length > 6 || inputValue.length < 5){
+       alert(inputValue + " is not a five or six-digit number.");
+   } else if (inputValue.length == 6 && inputValue > 0){
+       alert(inputValue + " is not a five-digit number."); */
 
 
-function minOfDay(){
+function minOfDay() {
     var currentMinute = new Date().getHours() * 60 + new Date().getMinutes();
     alert("Current Minute of the Day: " + currentMinute);
 }
 
-function disappear(){
+function disappear() {
     var dropdown = document.getElementById('navbarDropdown')
     dropdown.style.display = "none";
 }
